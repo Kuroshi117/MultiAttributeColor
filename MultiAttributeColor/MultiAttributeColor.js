c1d9bd20e69a57dc2f4fc85be1df1b5e94898806
Phoebe Huang <phoebehana4@gmail.com>
2018-09-27 23:22:27 -0500

    Needed a precision mediump float in FSHADER

--- MultiAttributeColor/MultiAttributeColor.js
+++ MultiAttributeColor/MultiAttributeColor.js
@@ -3,17 +3,18 @@
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
     'attribute vec4 a_Color;\n' +
-    'varying vec4 v_Color'+
+    'varying vec4 v_Color;\n'+
     'void main(){\n' +
     '   gl_Position=a_Position;\n' +
-    '   gl_PointSize=10;\n' +
+    '   gl_PointSize=10.0;\n' +
     '   v_Color=a_Color;\n'+
     '}\n';
 
 var FSHADER_SOURCE =
-    'varying vec4 v_Color;\n'+
+    'precision mediump float;\n' +
+    'varying vec4 v_Color;\n' +
     'void main() {\n' +
-    '	gl_FragColor= v_Color;\n' +
+    '	gl_FragColor = v_Color;\n' +
     '}\n';
 
 function main() {
@@ -40,23 +41,22 @@ function main() {
 }
 
 function initVertexBuffers(gl) {
-    var verticesColor = new Float32Array([
+    var verticesColors = new Float32Array([
         0.0, 0.5, 1.0, 0.0, 0.0,
         -0.5, -0.5, 0.0, 1.0, 0.0,
-        0.5, - 0.5, 0.0, 0.0, 1.0, 
+        0.5, -0.5, 0.0, 0.0, 1.0,
     ]);
     var n = 3;
 
-
     var vertexColorBuffer = gl.createBuffer();
     if (!vertexColorBuffer) {
-        console.log('Failed to create buffer object');
-        return -1;
+        console.log('Failed to create the buffer object');
+        return false;
     }
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, verticesColor, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
 
-    var FSIZE = verticesColor.BYTES_PER_ELEMENT;
+    var FSIZE = verticesColors.BYTES_PER_ELEMENT;
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     if (a_Position < 0) {
         console.log('Failed to get the storage location of a_Position');
@@ -65,10 +65,9 @@ function initVertexBuffers(gl) {
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
     gl.enableVertexAttribArray(a_Position);
 
-
     var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
     if (a_Color < 0) {
-        console.log('Failed to get the storage location of a_PointSize');
+        console.log('Failed to get the storage location of a_Color');
         return -1;
     }
     gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
